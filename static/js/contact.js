0366559e0261f9f153c669fc8cfa5bd6ce95bd9c
houstonhaynes <h3techsme@gmail.com>
2019-10-12 13:01:35 -0700

    Update from submodule instance

---
+++ static/js/contact.js
@@ -0,0 +1,40 @@
+document.getElementById('submitMessage').addEventListener('submit', submitMessage);
+function processResponse(response) {
+    if (response.status === 200) {
+        output =
+            `
+        <div class="alert alert-success" role="alert">
+          Thanks, ${document.getElementById('name').value}! We'll be in touch soon!
+        </div>        
+        `;
+        document.getElementById('output').innerHTML = output;
+        document.getElementById('submitMessage').style.visibility = "hidden";
+    } else {
+        output =
+            `
+        <div class="alert alert-danger" role="alert">
+          Oh no! Something went wrong :(
+        </div>        
+        `;
+        document.getElementById('output').innerHTML = output;
+    }
+}
+function submitMessage(e) {
+    e.preventDefault();
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    fetch('https://h3tech.azurewebsites.net/api/contact', {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            message: message
+        })
+    })
+    .then((res) => processResponse(res))
+}
