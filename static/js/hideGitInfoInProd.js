26e4a7403df43a87de5a349a40974e0d715996f8
houstonhaynes <h3techsme@gmail.com>
2019-10-17 22:05:43 -0700

    GitInfo table format, listener to hide in prod

---
+++ static/js/hideGitInfoInProd.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    if (window.location.href.indexOf("http://h3tech.io") != -1) {
+        document.getElementById('GitInfo').style.visibility = "hidden";
+    }
+});
