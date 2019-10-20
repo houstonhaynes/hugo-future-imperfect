d621ec248b49aa3cbbcb240a40053b24063cb9bd
houstonhaynes <h3techsme@gmail.com>
2019-10-20 15:14:48 -0700

    bug in URL

--- static/js/hideGitInfoInProd.js
+++ static/js/hideGitInfoInProd.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    if (window.location.href.indexOf("http://h3tech.io") != -1) {
+    if (window.location.href.indexOf("https://h3tech.io") != -1) {
         document.getElementById('GitInfo').style.visibility = "hidden";
     }
 });
